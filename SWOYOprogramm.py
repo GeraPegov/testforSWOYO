5be861ed6671bd388b4c64327fa2e0f20df8bcd0
Gera <gera51666@gmail.com>
2025-04-08 10:47:52 +0300

    last

---
+++ SWOYOprogramm.py
@@ -0,0 +1,82 @@
+import socket 
+import argparse 
+from  classforCLI import Request, Responses
+from log import setup_logger
+import logging
+import toml
+
+setup_logger()
+logger=logging.getLogger('MyProgramm')
+logger.info('programm is ready')
+
+
+parser = argparse.ArgumentParser(
+    prog="SMS sender CLI", 
+    description="Программа для отправки сообщений", 
+    usage='%(prog)s sender recipient message'
+)
+parser.add_argument('sender', type=str, help='Номер отправителя')
+parser.add_argument('recipient', type=str, help='Номер получателя')
+parser.add_argument('message', type=str, help='Текст сообщения')
+
+# Вывод справки  если аргументы не переданы
+import sys
+if len(sys.argv) == 1:
+    parser.print_help()
+    sys.exit(1)
+
+args = parser.parse_args()
+#формирование словаря из полученных данных от пользователя
+body: dict[str, str] = vars(args)
+logger.info(f'Получены параметры: {body}')
+for_classCLI = Request(body)
+logger.info(f'Создан объект запроса с параметрами: {body}')
+
+with open("config.toml") as f:
+    data = toml.load(f)
+url = data['URL']
+host_url = url.split('/')[2].split(':')[0]
+port_url = int(''.join(url.split('/')[2].split(':')[1]))
+
+try:
+    #создание сокета
+    sock = socket.socket() 
+    sock.settimeout(10)  # 10 секунд таймаут
+    #подключение к порту
+    sock.connect((host_url, port_url))
+    logger.info(f'Подключение к порту {host_url, port_url}')
+    #отправка запроса в байтах 
+    send_message = for_classCLI.to_bytes()
+    logger.info(f'HTTP {send_message.decode()}')
+    sock.send(send_message)
+    logger.info(f'Отправка запроса в байтах {send_message}')
+    #возвращаемый ответ 
+    reply = sock.recv(4096)
+
+    #декодируем ответ и отправляем в вывод  
+    decode_bytes = Responses()
+    decode_bytes.from_bytes(reply)
+    answer_from_server = decode_bytes.from_bytes(reply)
+    logger.info(f'Декодируемый вывод {answer_from_server}')
+
+    print(answer_from_server)
+except socket.timeout:
+    logger.error('Превышено время ожидания ответа от сервера')
+    print('Ошибка: Превышено время ожидания ответа от сервера')
+except Exception as e:
+    logger.error(f'Непредвиденная ошибка: {e}')
+    print(f'Ошибка: {e}')
+except ConnectionError as e:
+    logger.error(f'Ошибка подключения: {e}')
+    print(f'Ошибка: Не удалось подключиться к серверу. {e}')
+except socket.gaierror:
+    logger.error(f'Ошибка разрешения имени хоста: {host_url}')
+    print(f'Ошибка: Не удалось разрешить имя хоста {host_url}')
+finally:
+    try:
+        sock.close()
+        logger.info('Соединение закрыто')
+    except:
+        pass
+
+
--- classforCLI.py
+++ classforCLI.py
@@ -3,23 +3,25 @@ from base64 import b64encode
 import toml
 
 class Request:
-    with open("config.toml") as f:
-        data = toml.load(f)
-    def __init__(self, body: list):
+    def __init__(self, body: dict):
         self.sender = body['sender']
         self.recipient = body['recipient']
         self.message = body['message']
-        self.url = self.data['URL']
-        self.password = self.data['password']
-        self.name = self.data['user']
+
+        with open("config.toml") as f:  
+            data = toml.load(f)
+        self.url = data['URL']
+        self.password = data['password']
+        self.name = data['user']
 
     def to_bytes(self) -> bytes:
-        auth_str = f'self.password:self.name'
+        auth_str = f'{self.name}:{self.password}'
         basicAuth = b64encode(auth_str.encode()).decode()
 
-        host = self.url.split()[0]
-        path = "/send_sms"
-
+        host = self.url.split('/')[2].split(':')[0]
+        path = f'/{self.url.split('/')[3]}'
+        if not path:
+            path = "/send_sms"
         json_body = json.dumps({
             'sender': self.sender,
             'recipient': self.recipient,
@@ -35,28 +37,21 @@ class Request:
             f"\r\n"
             f"{json_body}"
         )
-        print(http_request)
+        
         return http_request.encode()
     
 class Responses:
     def from_bytes(self, reply: bytes) -> str:
-        text_reply = list(reply) 
-        resp_code = []
-        body_text = []
-        for i in text_reply[9:12]:
-            resp_code.append(i)
-
-        start = text_reply.index(123)
-        for i in text_reply[start:]:
-            body_text.append(i)
-
-        return f'code: {bytes(resp_code).decode()}, body: {bytes(body_text).decode()}'
-        # text = back.decode().split('\r\n')
-        # resp_code = text[0]
-        # body_text = text[-1]
-        # return f'code: {resp_code}, body: {body_text}'
+        decode_reply = reply.decode().split('\r\n')
+        code = decode_reply[0]
+        body = decode_reply[-1] 
+        body = body.replace('{', '')
+        body = body.replace('}', '')
+        body = body.replace('"', '')
+        body = body.replace(',', ", ")
         
-            
+        return f'Status code = {' '.join(code.split(' ')[1:])}\nBody = {body}'
+        
 
         
 
